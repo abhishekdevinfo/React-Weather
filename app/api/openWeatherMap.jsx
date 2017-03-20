68e60843853fd43a188a4010a54a530f32c250eb
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-03-20 21:17:07 +0530

    Added Modal for error

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -6,14 +6,14 @@ module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
       }
-    }, function (res) {
+    })
+    .catch(function (res) {
       throw new Error(res.data.message);
     });
   }
