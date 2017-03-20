0db5b3b25baa0ba9cdb47ecfdab7fde1e10f0c3b
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-03-20 13:46:31 +0530

    update server file

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.header['x-forward-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url)
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.header['x-forward-proto'] === 'http') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url)
+//   }
+// });
 
 app.use(express.static('public'));
 
